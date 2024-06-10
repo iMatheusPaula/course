45ea4188e49f8d9a064978c6eae7c205fb218a92
iMatheusPaula <imatheuspaula@gmail.com>
2024-06-09 23:54:15 -0300

    class directory management

--- 03-manipulacao-e-tratamento/03-08-gestao-de-diretorios/index.php
+++ 03-manipulacao-e-tratamento/03-08-gestao-de-diretorios/index.php
@@ -7,14 +7,60 @@ fullStackPHPClassName("03.08 - Gestão de diretórios");
  */
 fullStackPHPClassSession("verificar, criar e abrir", __LINE__);
 
+$folder = __DIR__ . "/uploads";
+
+if(!file_exists($folder) && !is_dir($folder)) mkdir($folder, 0755);
+else var_dump(scandir($folder));
 
 /*
  * [ copiar e renomear ] copy | rename
  */
 fullStackPHPClassSession("copiar e renomear", __LINE__);
 
+$file = __DIR__ . "/file.txt";
+if(!file_exists($file) && !is_dir($file) && !is_file($file)) {
+    $open = fopen($file, "w");
+    fclose($open);
+} else {
+//    var_dump(filemtime($file), filemtime(__DIR__ . "/uploads/file.txt"));
+//    copy($file, $folder . "/" . basename($file));
+//    rename(
+//        __DIR__ . "/uploads/file.txt",
+//        __DIR__ . "/uploads/".time(). ".".pathinfo($file, PATHINFO_EXTENSION)
+//    );
+
+    rename($file, __DIR__ . "/uploads/".time(). ".".pathinfo($file, PATHINFO_EXTENSION));
+}
+
+var_dump(pathinfo($file), scandir($folder), scandir(__DIR__));
 
 /*
  * [ remover e deletar ] unlink | rmdir
  */
 fullStackPHPClassSession("remover e deletar", __LINE__);
+
+//mkdir("remove", 0755);
+$dir = __DIR__ . "/remove";
+
+//rmdir($dir);
+
+$dirFiles = array_diff(scandir($dir), ['.', '..']);
+$count = count($dirFiles);
+
+if($count > 0) {
+    foreach(scandir($dir) as $file) {
+        $file = __DIR__ . "/remove/{$file}";
+        if(file_exists($file) && is_file($file)) {
+            unlink($file);
+        }
+    }
+    rmdir($dir);
+}
+else {
+    rmdir($dir);
+}
+var_dump($dirFiles, $count);
+
+
+
+
